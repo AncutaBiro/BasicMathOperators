23cc41b653bc50ad87953de9546b286c633c39f0
AncutaBiro <ancuta.biro@yahoo.com>
2020-03-21 22:17:35 +0200

    added method overloading

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -10,30 +10,41 @@ public class App
     {
         User user = new User();
 
-        int sum = user.add(5,10);
-        System.out.println("The sum is: " + sum);
+        int sumInt = user.add(5,10);
+        System.out.println("The sumInt is: " + sumInt);
+        double sumDouble = user.add (5.2,3.6);
+        System.out.println("The sumDouble is: " + sumDouble);
 
-        double difference = user.subtract(5.5,1.3);
-        System.out.println("The difference is: " + difference);
+        int differenceInt = user.subtract(5,1);
+        System.out.println("The differenceInt is: " + differenceInt);
+        double differenceDouble = user.subtract(5.5,1.3);
+        System.out.println("The differenceDouble is: " + differenceDouble);
 
-        int product = user.multiplication(5,10);
-        System.out.println("The product is: " + product);
+        int productInt = user.multiplication(5,10);
+        System.out.println("The productInt is: " + productInt);
+        double productDouble = user.multiplication(5.1,10.1);
+        System.out.println("The productDouble is: " + productDouble);
 
-        int quotient = user.divide(50,10);
-        System.out.println("The quotient is: " + quotient);
+        int quotientInt = user.divide(50,10);
+        System.out.println("The quotientInt is: " + quotientInt);
+        double quotientDouble = user.divide(50.0,10.0);
+        System.out.println("The quotientDouble is: " + quotientDouble);
 
-        int remainder = user.modulo(5,3);
-        System.out.println("The remainder is: " + remainder);
+        int remainderInt = user.modulo(5,3);
+        System.out.println("The remainderInt is: " + remainderInt);
+        double remainderDouble = user.modulo(5.0,3.0);
+        System.out.println("The remainderDouble is: " + remainderDouble);
 
-        int result1 = user.calculate1(2,5,3);
-        System.out.println("The result of the operation is: " + result1);
+        int result1Int = user.calculate1(2,5,3);
+        System.out.println("The result of the operation is: " + result1Int);
+        double result1Double = user.calculate1(3.2,5,3);
+        System.out.println("The result of the operation is: " + result1Double);
 
-        int result2 = user.calculate2(10,5,3);
-        System.out.println("The result of the operation is: " + result2);
-
+        int result2Int = user.calculate2(10,5,3);
+        System.out.println("The result of the operation is: " + result2Int);
+        double result2Double = user.calculate2(10,5,3.5);
+        System.out.println("The result of the operation is: " + result2Double);
     }
-
-
 
 }
 
--- src/main/java/org/example/User.java
+++ src/main/java/org/example/User.java
@@ -2,10 +2,17 @@ package org.example;
 
 public class User {
 
+
     public int add (int firstValue, int secondValue) {
         return firstValue + secondValue;
     }
+    public double add (double firstValue, double secondValue) {
+        return firstValue + secondValue;
+    }
 
+    public int subtract (int firstValue, int secondValue) {
+        return firstValue - secondValue;
+    }
     public double subtract (double firstValue, double secondValue) {
         return firstValue - secondValue;
     }
@@ -13,20 +20,36 @@ public class User {
     public int multiplication (int firstValue, int secondValue) {
         return firstValue * secondValue;
     }
+    public double multiplication (double firstValue, double secondValue) {
+        return firstValue * secondValue;
+    }
 
     public int divide (int firstValue, int secondValue) {
+        return firstValue / secondValue;
+    }
+    public double divide (double firstValue, double secondValue) {
         return firstValue / secondValue;
     }
 
     public int modulo (int firstValue, int secondValue) {
         return firstValue % secondValue;
     }
+    public double modulo (double firstValue, double secondValue) {
+        return firstValue % secondValue;
+    }
 
     public int calculate1 (int firstValue, int secondValue, int thirdValue) {
         return firstValue + secondValue * thirdValue;
     }
+    public double calculate1 (double firstValue, int secondValue, int thirdValue) {
+        return firstValue + secondValue * thirdValue;
+    }
 
     public int calculate2 (int firstValue, int secondValue, int thirdValue) {
+        thirdValue--;
+        return firstValue * secondValue + thirdValue;
+    }
+    public double calculate2 (int firstValue, int secondValue, double thirdValue) {
         thirdValue--;
         return firstValue * secondValue + thirdValue;
     }
